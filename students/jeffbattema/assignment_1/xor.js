a383a3bc6cb306e59e3fff0e1abb75c2a9d667eb
Jeffrey Battema <jeff@cyberwocky.com>
2014-07-09 20:48:55 -0700

    reverting assignment 1

--- students/jeffbattema/assignment_1/xor.js
+++ students/jeffbattema/assignment_1/xor.js
@@ -1,23 +1,4 @@
-var firstName, lastName;
-
 function preferredName (arg1, arg2)
 {
-
-return ( (arg1 && !arg2) || (!arg1 && arg2) ? (arg1 || arg2) : false);
-
+return (!arg1 == !arg2) ? false : (arg1 || arg2);
 }
-
-
-firstName = "Hank";
-
-preferredName (firstName, lastName);
-
-lastName = 'Yates';
-
-preferredName (firstName, lastName);
-// -> false
-
-firstName = '';
-
-preferredName (firstName, lastName);
-// -> 'Yates'
